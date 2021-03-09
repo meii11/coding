0da799f6066b114ace9432e79435dc44a7ec9cee
meii <meii11cao@gmail.com>
2021-03-09 10:16:51 +0800

    3.9

--- c_prime_plus/7/7_9.c
+++ c_prime_plus/7/7_9.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void main(){
-    int i_num;
-    printf("Please enter a positive number: \n");
-    scanf("%d", &i_num);
+int main(void)
+{
+	unsigned int num;
+	unsigned int div;
+	bool isPrimer = true;
 
-    for(int i=1;i<=i_num;i++){
-        if()
+	printf("Enter a number (q to quit ): \n");
+	while (scanf("%u", &num) == 1)
+	{
+	    if (num <= 1)
+            printf("No primes.\n");
+        else
+        {
+
+            for (int i = 2; i <= num; i++)
+            {
+                isPrimer = true;
+                for (div = 2; div * div <= i; div++)
+                {
+                    if (i % div == 0)
+                    {
+                        isPrimer = false;
+                    }
+                }
+                if (isPrimer)
+                    printf("%u is Primer \n", i);
+            }
+        }
+        printf("Enter a number (q to quit ): \n");
     }
+	printf("Done! \n");
+
+	return 0;
 }
