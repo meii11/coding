fad25efdfb5d824f9d4123a60bb82ee24d0c2741
meii <meii11cao@gmail.com>
2021-03-22 21:42:42 +0800

    3.22

---
+++ cpp_prime_plus/3/3_01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    int temp(2);
+    int temp2 = 2;
+    cout << temp << endl << temp2 << endl;
+    cout.put('$');
+    cout << "Done!";
+}
