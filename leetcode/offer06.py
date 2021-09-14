8f4ffc213d9b700846d68ddfeca263c046d5d164
meiiCao <meii11cao@gmail.com>
2021-09-14 14:56:44 +0800

    9-14

--- leetcode/offer06.py
+++ leetcode/offer06.py
@@ -9,7 +9,7 @@ class Solution:
 
         return hashtable[-1::-1]
 
-# 递归
+# 递归，感觉递归的本质就是栈，先进后出
 class Solution:
     def reversePrint(self, head: ListNode) -> List[int]:
         return self.reversePrint(head.next) + [head.val] if head else []
